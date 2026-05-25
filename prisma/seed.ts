4af4f3936121bf5878817a35cf09936c3797858c
Cyborg <edisonelvisy@gmail.com>
2026-05-25 17:28:44 +0000

    fixed prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma } from "../lib/prisma";
 import bcrypt from "bcryptjs";
+import { }
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL,
+});
 
 async function main() {
   const adminPassword = await bcrypt.hash("admin123", 12);
